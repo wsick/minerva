ed59f84f43b6a75529860d32bb760a82fd213d44
BSick7 <brad.sickles@gmail.com>
2014-09-11 08:42:14 -0400

    Implemented pretransformMatrix.

--- src/Render/RenderContext.ts
+++ src/Render/RenderContext.ts
@@ -1,7 +1,13 @@
+interface CanvasRenderingContext2D {
+    currentTransform: number[];
+}
+
 module minerva.render {
     export class RenderContext {
-        static pretransformMatrix (ctx: CanvasRenderingContext2D, xform: number[]) {
-
+        static pretransformMatrix (ctx: CanvasRenderingContext2D, mat: number[]) {
+            var ct = ctx.currentTransform;
+            mat3.multiply(mat, ct, ct);
+            ctx.setTransform(ct[0], ct[1], ct[3], ct[4], ct[2], ct[5]);
         }
 
         static clipGeometry (ctx: CanvasRenderingContext2D, geom: IGeometry) {
