5a55e37cf75d14b88a6486f7269f4738c7d04cfb
BSick7 <brad.sickles@gmail.com>
2014-09-11 21:33:36 -0400

    Unit testing validateRegion.

--- test/tests/render.ts
+++ test/tests/render.ts
@@ -4,10 +4,11 @@
 QUnit.module("render");
 
 import render = minerva.def.render;
+import tapins = minerva.def.render.tapins;
 import Rect = minerva.Rect;
 
-QUnit.test("validate", function (assert) {
-    var assets:render.IAssets = {
+QUnit.test("validate", (assert) => {
+    var assets: render.IAssets = {
         TotalIsRenderVisible: true,
         TotalOpacity: 1.0,
         SurfaceBoundsWithChildren: new Rect(),
@@ -15,19 +16,53 @@ QUnit.test("validate", function (assert) {
         Clip: null,
         Effect: null
     };
-    var state:render.IState = {
+    var state: render.IState = {
         RenderRegion: new Rect()
     };
 
-    assert.ok(render.tapins.validate(assets, state, null, null, null), "#1");
+    assert.ok(tapins.validate(assets, state, null, null, null), "#1");
 
     assets.TotalIsRenderVisible = false;
-    assert.ok(!render.tapins.validate(assets, state, null, null, null), "#2");
+    assert.ok(!tapins.validate(assets, state, null, null, null), "#2");
 
     assets.TotalIsRenderVisible = true;
     assets.TotalOpacity = 0.002;
-    assert.ok(render.tapins.validate(assets, state, null, null, null), "#3");
+    assert.ok(tapins.validate(assets, state, null, null, null), "#3");
 
     assets.TotalOpacity = 0.0018;
-    assert.ok(!render.tapins.validate(assets, state, null, null, null), "#4");
+    assert.ok(!tapins.validate(assets, state, null, null, null), "#4");
+});
+
+QUnit.test("validateRegion", (assert) => {
+    var assets: render.IAssets = {
+        TotalIsRenderVisible: true,
+        TotalOpacity: 1.0,
+        SurfaceBoundsWithChildren: new Rect(50, 50, 100, 100),
+        RenderXform: mat3.identity(),
+        Clip: null,
+        Effect: null
+    };
+    var state: render.IState = {
+        RenderRegion: new Rect()
+    };
+
+    assert.ok(tapins.validateRegion(assets, state, null, null, new Rect(0, 0, 100, 200)), "#1");
+    var rr = state.RenderRegion;
+    assert.equal(rr.x, 50);
+    assert.equal(rr.y, 50);
+    assert.equal(rr.width, 50);
+    assert.equal(rr.height, 100);
+
+
+    assert.ok(!tapins.validateRegion(assets, state, null, null, new Rect(0, 0, 50, 150)), "#2");
+    assert.equal(rr.x, 50);
+    assert.equal(rr.y, 50);
+    assert.equal(rr.width, 0);
+    assert.equal(rr.height, 100);
+
+    assert.ok(!tapins.validateRegion(assets, state, null, null, new Rect(100, 150, 25, 10)), "#3");
+    assert.equal(rr.x, 100);
+    assert.equal(rr.y, 150);
+    assert.equal(rr.width, 25);
+    assert.equal(rr.height, 0);
 });
