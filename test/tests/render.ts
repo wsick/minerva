509be790616313c508ab5abff9db0fda22ac6822
BSick7 <brad.sickles@gmail.com>
2014-09-13 08:52:33 -0400

    Finished unit testing render tapins.  Still need to implement and test renderChildren.

--- test/tests/render.ts
+++ test/tests/render.ts
@@ -7,7 +7,7 @@ import render = minerva.def.render;
 import tapins = minerva.def.render.tapins;
 import Rect = minerva.Rect;
 
-function typedToArray(typed) {
+function typedToArray (typed) {
     var arr = [];
     for (var i = 0; i < typed.length; i++) {
         arr.push(typed[i]);
@@ -16,7 +16,7 @@ function typedToArray(typed) {
 }
 
 var mock = {
-    assets: function():render.IAssets {
+    assets: function (): render.IAssets {
         return <render.IAssets> {
             TotalIsRenderVisible: true,
             TotalOpacity: 1.0,
@@ -26,7 +26,7 @@ var mock = {
             Effect: null
         };
     },
-    state: function(): render.IState {
+    state: function (): render.IState {
         return <render.IState> {
             RenderRegion: new Rect()
         };
@@ -94,4 +94,124 @@ QUnit.test("prepareContext", (assert) => {
     render.RenderContext.restore(ctx);
     assert.equal(ctx.globalAlpha, 1.0);
     assert.deepEqual(typedToArray(ctx.currentTransform), [2, 0, 0, 0, 4, 0, 0, 0, 1]);
+});
+
+QUnit.test("applyClip", (assert) => {
+    var assets = mock.assets();
+    var state = mock.state();
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    render.RenderContext.init(ctx);
+
+    var clipDrawn = false;
+    assets.Clip = <minerva.def.render.IGeometry>{
+        Draw: function (ctx: CanvasRenderingContext2D) {
+            clipDrawn = true;
+        }
+    };
+
+    var clipped = false;
+    ctx.clip = function () {
+        clipped = true;
+    };
+
+    assert.ok(tapins.applyClip(assets, state, null, ctx, new Rect(), "#1"));
+    assert.ok(clipDrawn, "#2");
+    assert.ok(clipped, "#3");
+});
+
+QUnit.test("preRender", (assert) => {
+    var assets = mock.assets();
+    var state = mock.state();
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    render.RenderContext.init(ctx);
+
+    var tmp = render.RenderContext.save;
+    var saved = false;
+    render.RenderContext.save = function () {
+        tmp.apply(this, arguments);
+        saved = true;
+    };
+
+    assets.Effect = <minerva.def.render.IEffect> {
+        PreRender: function (ctx: CanvasRenderingContext2D) {
+            ctx.globalAlpha = 0.5;
+        },
+        PostRender: function (ctx: CanvasRenderingContext2D) {
+
+        }
+    };
+    assert.ok(tapins.preRender(assets, state, null, ctx, new Rect(), "#1"));
+    assert.ok(saved);
+    assert.equal(ctx.globalAlpha, 0.5);
+    render.RenderContext.restore(ctx);
+    assert.equal(ctx.globalAlpha, 1.0);
+
+    saved = false;
+    assets.Effect = null;
+    assert.ok(tapins.preRender(assets, state, null, ctx, new Rect(), "#2"));
+    assert.ok(!saved);
+});
+
+QUnit.test("doRender", (assert) => {
+    ok(true);
+});
+
+QUnit.test("postRender", (assert) => {
+    var assets = mock.assets();
+    var state = mock.state();
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    render.RenderContext.init(ctx);
+
+    var tmp = render.RenderContext.restore;
+    var restored = false;
+    render.RenderContext.restore = function () {
+        tmp.apply(this, arguments);
+        restored = true;
+    };
+
+    assets.Effect = <minerva.def.render.IEffect> {
+        PreRender: function (ctx: CanvasRenderingContext2D) {
+        },
+        PostRender: function (ctx: CanvasRenderingContext2D) {
+            ctx.globalAlpha = 0.5;
+        }
+    };
+    render.RenderContext.save(ctx);
+    assert.ok(tapins.postRender(assets, state, null, ctx, new Rect(), "#1"));
+    assert.ok(restored);
+    assert.equal(ctx.globalAlpha, 1.0);
+
+    restored = false;
+    assets.Effect = null;
+    assert.ok(tapins.postRender(assets, state, null, ctx, new Rect(), "#2"));
+    assert.ok(!restored);
+});
+
+QUnit.test("renderChildren", (assert) => {
+    ok(true);
+});
+
+QUnit.test("restoreContext", (assert) => {
+    var assets = mock.assets();
+    var state = mock.state();
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    render.RenderContext.init(ctx);
+
+    var tmp = render.RenderContext.restore;
+    var restored = false;
+    render.RenderContext.restore = function () {
+        tmp.apply(this, arguments);
+        restored = true;
+    };
+
+    assert.ok(tapins.restoreContext(assets, state, null, ctx, new Rect(), "#1"));
+    assert.ok(restored);
 });
