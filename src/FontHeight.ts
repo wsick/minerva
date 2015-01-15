d298bd836bdd4c10cde717f9455b0abfbc0c1014
BSick7 <brad.sickles@gmail.com>
2015-01-15 14:26:34 -0500

    Better font caching.

---
+++ src/FontHeight.ts
@@ -0,0 +1,36 @@
+module minerva.fontHeight {
+    var heights = [];
+
+    export var cache = {
+        hits: 0,
+        misses: 0
+    };
+
+    export function get (font: Font): number {
+        var serial = font.toHtml5Object();
+        var height = heights[serial];
+        if (height == null) {
+            heights[serial] = height = measure(serial);
+            cache.misses++;
+        } else {
+            cache.hits++;
+        }
+        return height;
+    }
+
+
+    var dummy: HTMLElement;
+
+    function measure (serial: string): number {
+        if (!dummy) {
+            dummy = document.createElement("div");
+            dummy.appendChild(document.createTextNode("M"));
+            document.body.appendChild(dummy);
+        }
+        dummy.style.display = "";
+        dummy.style.font = serial;
+        var result = dummy.offsetHeight;
+        dummy.style.display = "none";
+        return result;
+    }
+}
